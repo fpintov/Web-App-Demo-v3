53678fb6e67a2469168a005648477c65a8a3f094
Francisco J. Pinto Villar <fpintov1@gmail.com>
2022-06-13 14:10:33 -0400

    Añadiendo formato al SideBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { UsersComponent } from './users/users.component';
+import { PagesComponent } from './pages/pages.component';
+import { MediaComponent } from './media/media.component';
+import { SettingsComponent } from './settings/settings.component';
+import { AbonosComponent } from './abonos/abonos.component';
+import { EgresosComponent } from './egresos/egresos.component';
+import { InformesComponent } from './informes/informes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    SidenavComponent,
+    DashboardComponent,
+    StatisticsComponent,
+    UsersComponent,
+    PagesComponent,
+    MediaComponent,
+    SettingsComponent,
+    AbonosComponent,
+    EgresosComponent,
+    InformesComponent
   ],
   imports: [
     BrowserModule
