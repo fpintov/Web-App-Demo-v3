a71d3614e8ea9ca7ef292f8f5a23fa6815bbd24d
Francisco J. Pinto Villar <fpintov1@gmail.com>
2022-06-13 21:58:14 -0400

    Proyecto base con formatos terminados, faltan correcciones

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,4 +1,5 @@
-import { Component, Output, EventEmitter} from '@angular/core';
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
+import { Component, Output, EventEmitter, HostListener} from '@angular/core';
 import { navbarData } from './nav-data';
 
 interface SideNavToggle{
@@ -9,7 +10,31 @@ interface SideNavToggle{
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.css']
+  styleUrls: ['./sidenav.component.css'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({opacity: 0}), 
+        animate('350ms'), 
+          style({opacity: 1})
+      ]),
+      transition(':leave', [
+        style({opacity: 1}), 
+        animate('350ms'), 
+          style({opacity: 0})
+      ])
+    ]),
+    trigger('rotate', [
+      transition(':enter', [
+        animate('1000ms', 
+          keyframes([
+            style({transform: 'rotate(0deg)', offset: '0'}), 
+            style({transform: 'rotate(2turn)', offset: '1'})
+          ])
+        )
+      ])
+    ])
+  ]
 })
 
 export class SidenavComponent {
@@ -19,6 +44,19 @@ export class SidenavComponent {
   collapsed = false;
   screenWidth = 0;
   navData = navbarData;
+
+  @HostListener('window:resize', ['$event']) 
+  onResize(event: any){
+    this.screenWidth = window.innerWidth;
+    if(this.screenWidth <= 768){
+      this.collapsed = false;
+      this.onToggleSidenav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+    }
+  }
+
+  ngOnInit(): void{
+    this.screenWidth = window.innerWidth;
+  }
 
   toggleCollapse(): void{
     this.collapsed = !this.collapsed;
